3f76683eab7073ab5f0090c9c93188aac89d867b
isaacsousa <isaacsousa@hotmail.com>
2015-02-05 01:50:28 +0000

    New Structure

--- first/assets/js/app.js
+++ first/assets/js/app.js
@@ -3,3 +3,9 @@ var ContactManager = new Marionette.Application();
 ContactManager.addRegions({
 	mainRegion: "#main-region"
 });
+
+ContactManager.on("start", function() {
+	console.log("ContactManager has started!");
+
+	ContactManager.ContactsApp.List.Controller.listContacts();
+});
